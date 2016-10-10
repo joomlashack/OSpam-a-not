04836c50fa7f6941aec76e6391a6e3be49e78ef1
Anderson Grudtner Martins <anderson@ostraining.com>
2016-10-10 20:14:23 -0300

    Joomlashack rebrand

--- src/Method/HoneyPot.php
+++ src/Method/HoneyPot.php
@@ -2,7 +2,7 @@
 /**
  * @package    OSpam-a-not
  * @subpackage
- * @contact    www.ostraining.com, support@ostraining.com
+ * @contact    www.joomlashack.com, help@joomlashack.com
  * @copyright  2015 Open Source Training, LLC. All rights reserved
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
--- src/include.php
+++ src/include.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package   OSpam-a-not
- * @contact   www.alledia.com, support@alledia.com
- * @copyright 2015 Alledia.com, All rights reserved
+ * @contact   www.joomlashack.com, support@joomlashack.com
+ * @copyright 2015 joomlashack.com, All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
