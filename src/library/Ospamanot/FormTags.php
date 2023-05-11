53e7a4848495dbc10da45cc9282ed4e9644bb359
Bill Tomczak <bill@joomlashack.com>
2023-05-11 11:11:20 -0700

    Update file headers

--- src/library/Ospamanot/FormTags.php
+++ src/library/Ospamanot/FormTags.php
@@ -2,7 +2,7 @@
 /**
  * @package   OSpam-a-not
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2020-2022 Joomlashack.com. All rights reserved
+ * @copyright 2020-2023 Joomlashack.com. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of OSpam-a-not.
