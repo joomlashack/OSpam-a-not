c9827b4876521ecf421a719929f192277688fd42
Bill Tomczak <bill@joomlashack.com>
2025-12-05 09:44:27 -0800

    Update file headers

--- src/library/alledia/Ospamanot/Filter/AbstractFilter.php
+++ src/library/alledia/Ospamanot/Filter/AbstractFilter.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * @package   OSpam-a-not
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2023 Joomlashack.com. All rights reserved
+ * @copyright 2023-2026 Joomlashack.com. All rights reserved
  * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of OSpam-a-not.
