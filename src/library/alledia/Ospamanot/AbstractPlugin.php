d6b2fdb1828d254c8063b1cf90305cdd8b00be55
Bill Tomczak <bill@joomlashack.com>
2026-01-22 12:06:50 -0800

    Bump dev version

---
+++ src/library/alledia/Ospamanot/AbstractPlugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package   OSpam-a-not
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2025 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL 
+ *
+ * This file is part of OSpam-a-not.
+ *
+ * OSpam-a-not is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OSpam-a-not is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OSpam-a-not.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// phpcs:disable PSR1.Files.SideEffects
+defined('_JEXEC') or die();
+// phpcs:enable PSR1.Files.SideEffects
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+
